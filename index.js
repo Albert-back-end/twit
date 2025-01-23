ca3de979518ff25d2f053b44015a07c574710d4c
Albert <albert231207@mail.ru>
2025-01-23 23:22:09 +0300

    Change project directory

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const PORT = 5000
+const app = express()
+
+app.use(express.json())
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.status(200).json('SERVER WORK 2')
+})
+app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
